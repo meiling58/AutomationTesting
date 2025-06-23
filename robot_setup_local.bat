097effcbd17afadf993b7cd31b0465487f862635
Mei-Ling Liang <meiling.liang58@gmail.com>
2025-06-23 10:28:03 -0400

    adding robot_setup_local bat file

---
+++ robot_setup_local.bat
@@ -0,0 +1,8 @@
+::Run these commands one time to set global security settings
+::{your_git_localtion} config --global user.email "{your_email}"
+::my sample:C:\Users\mliang>git config --global user.email "meiling.liang58@gmail.com"
+python -m pip install --upgrade pip
+python -m pip install --upgrade robotframework
+python -m pip install --upgrade selenium
+python -m pip install --upgrade robotframework-seleniumlibrary
+python -m pip install --upgrade webdriver-manager
